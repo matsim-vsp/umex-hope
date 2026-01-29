f6751ed8657307f870340a848439959a49ef9217
Sydpaltra <paltra@tu-berlin.de>
2026-01-29 15:09:40 +0100

    further work on event reader

--- events.jl
+++ events.jl
@@ -3,7 +3,7 @@ using DataFrames
 using XML
 using EzXML
 
-function event_reader(file_path)
+function event_reader(file_path, actType_or_person)
 
     """ Read an events file (xml), yielding each contained event.
         Events will be
@@ -20,27 +20,31 @@ function event_reader(file_path)
 
     # Process each event
     for event in events
-        # Extract attributes
 
-        #event = events[1]
-
-        if haskey(event, "actType")
-            act_type = event["actType"]
+        if haskey(event, actType_or_person)
+            act_type = event[actType_or_person]
             act_type = rsplit(act_type, '_', limit=2)[1]
         else
-            act_type = "no_actType"  # Or use: continue to skip these events
+            if actType_or_person == "actType"
+                act_type = "no_actType"  # Or use: continue to skip these events
+            end
+            if actType_or_person == "person"
+                act_type = "no_person"  # Or use: continue to skip these events
+            end
         end
         
-        # Collect all attributes as strings
-        attrs = Dict(attr.name => attr.content for attr in eachattribute(event))
-        
-        # Initialize DataFrame for this type if it doesn't exist
-        if !haskey(dfs_by_type, act_type)
-            dfs_by_type[act_type] = DataFrame()
+        if !occursin("pt_pt", act_type)
+            # Collect all attributes as strings
+            attrs = Dict(attr.name => attr.content for attr in eachattribute(event))
+            
+            # Initialize DataFrame for this type if it doesn't exist
+            if !haskey(dfs_by_type, act_type)
+                dfs_by_type[act_type] = DataFrame()
+            end
+            
+            # Append this event's data
+            push!(dfs_by_type[act_type], attrs, cols=:union)
         end
-        
-        # Append this event's data
-        push!(dfs_by_type[act_type], attrs, cols=:union)
     end
 
     return dfs_by_type
