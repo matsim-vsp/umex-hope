0371b03bfb25e881f8523b4d569f6779c0901ab0
Sydpaltra <paltra@tu-berlin.de>
2026-02-18 13:14:30 +0100

    Allows user to run network model locally

--- src/main/julia/run_locally.jl
+++ src/main/julia/run_locally.jl
@@ -1,16 +1,31 @@
 include("model.jl")
 include("../../../population.jl")
+include("../../../network_creation.jl")
 
-agent_attr = population_reader("006.output_persons.csv")
+pop_file = "006.output_persons.csv"
+agent_attr = population_reader(pop_file)
+network_file = "hannover-1pct.output_network.xml"
+network = network_creation(network_file)
+trajectories_file = "path"
+
 
 params = Dict(
     :seeds => 1,
     :iterations => 10,
-    :base_susceptibilities => [0.1],
+    :base_susceptibility => [0.1],
     :recovery_rate => 0.2,
     :days_necessary_exposure => 1,
-    :output_folder => missing,
-    :agent_attributes => agent_attr
+    :agent_attributes => agent_attr,
+    :health_status => 0,
+    :heat_exposure => 0, 
+    :days_exposed => 0,
+    :pregnancy => 0, 
+    :premorbidity => 0,
+    :pop_file => pop_file,
+    :network_file => network_file,
+    :network => network,
+    :trajectories_file => trajectories_file,
+    :output_folder => missing
 )
 
 run_model(params)
