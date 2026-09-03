c8298f92505d7d6c0c6b7d1d220738c1d1221610
Sydpaltra <paltra@tu-berlin.de>
2026-09-03 13:34:14 +0200

    more to preprocess only once/day

--- src/main/julia/run_locally.jl
+++ src/main/julia/run_locally.jl
@@ -11,9 +11,9 @@ include("postprocessing.jl")
 output_path = "data/" * replace(first(string(now()), 19), ":" => "")
 mkpath(output_path)
 
-utci_filename = "df_utci_$(Dates.format(today(), "yyyy-mm-dd")).csv"
+utci_filename = "input/df_utci_$(Dates.format(today(), "yyyy-mm-dd")).csv"
 if isfile(utci_filename)
-    df_utci = CSV.read(filename, DataFrame)
+    df_utci = CSV.read(utci_filename, DataFrame)
 else
     include("utci_prep.jl")
 end
@@ -25,22 +25,30 @@ network = network_creation(network_file)
 trajectories_file = "path"
 temperature_file = "TemperatureHannoverDWD.txt"
 temperature = temperature_reader(temperature_file)
-exp_plans_file = "../shared-svn/projects/umex-hope/data/dummy-output-1pct-0it/hannover-1pct.output_experienced_plans.xml.gz"
-exp_plans_pop_df, exp_plans_dict, exp_plans_durations_df = experienced_plans_reader(exp_plans_file)
-out_of_home_duration_df = process_all_agents(exp_plans_dict)
 
-agent_attr = leftjoin(agent_attr, out_of_home_duration_df, on = :person)
-agent_attr = leftjoin(agent_attr, exp_plans_durations_df, on = :person)
+agents_filename = "input/df_agents_attr_$(Dates.format(today(), "yyyy-mm-dd")).csv"
+if isfile(agents_filename)
+    agent_attr = CSV.read(agents_filename, DataFrame)
+else    
+    pop_file = "../shared-svn/projects/umex-hope/data/dummy-output-1pct-0it/hannover-1pct.output_persons.csv.gz"
+    agent_attr = population_reader(pop_file)
+    exp_plans_file = "../shared-svn/projects/umex-hope/data/dummy-output-1pct-0it/hannover-1pct.output_experienced_plans.xml.gz"
+    exp_plans_pop_df, exp_plans_dict, exp_plans_durations_df = experienced_plans_reader(exp_plans_file)
+    out_of_home_duration_df = process_all_agents(exp_plans_dict)
 
-#TODO: SOMETHING GOES WRONG WHEN COMPUTING TIME OF WALK, CHECK ONCE YOU ARE BACK FROM CONFERENCE
-cols = ["home", "educ", "errands", "pt", "bike", "visit", "shop", "work", "business", "walk", "leisure", "car", "accomp", "ride", "other"]
-agent_attr.total_hours = [sum(row[col] for col in cols) / 3600 for row in eachrow(agent_attr)]
-agent_attr_toolong = filter(row -> row.total_hours > 24, agent_attr)
-agent_attr_toosmall = filter(row -> row.total_hours < 23.5, agent_attr)
-filter!(row -> 23.5 <= row.total_hours <= 24, agent_attr)
-CSV.write(string(output_path, "/input_agent_attributes.csv"), agent_attr)
-CSV.write(string(output_path, "/input_agent_attributes_toolong.csv"), agent_attr_toolong)
-CSV.write(string(output_path, "/input_agent_attributes_toosmall.csv"), agent_attr_toosmall)
+    agent_attr = leftjoin(agent_attr, out_of_home_duration_df, on = :person)
+    agent_attr = leftjoin(agent_attr, exp_plans_durations_df, on = :person)
+
+    #TODO: SOMETHING GOES WRONG WHEN COMPUTING TIME OF WALK, CHECK ONCE YOU ARE BACK FROM CONFERENCE
+    cols = ["home", "educ", "errands", "pt", "bike", "visit", "shop", "work", "business", "walk", "leisure", "car", "accomp", "ride", "other"]
+    agent_attr.total_hours = [sum(row[col] for col in cols) / 3600 for row in eachrow(agent_attr)]
+    agent_attr_toolong = filter(row -> row.total_hours > 24, agent_attr)
+    agent_attr_toosmall = filter(row -> row.total_hours < 23.5, agent_attr)
+    filter!(row -> 23.5 <= row.total_hours <= 24, agent_attr)
+    CSV.write(string("input/", agents_filename), agent_attr)
+    CSV.write(string(output_path, "/input_agent_attributes_toolong.csv"), agent_attr_toolong)
+    CSV.write(string(output_path, "/input_agent_attributes_toosmall.csv"), agent_attr_toosmall)
+end 
 
 df_merged = preprocessing(df_merged, output_path)
 
